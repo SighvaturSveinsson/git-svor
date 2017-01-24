193648ef9daf9cfacd7eee0f1c1c80bd496f9b87
Sighvatur Sveinsson <sighvatursv373@nemi.tskoli.is>
2017-01-24 17:22:51 +0000

    1 spurning

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# git-svor
+#Git verkefni
---
+++ hvati.py
@@ -0,0 +1,11 @@
+#Python verkefni
+#Sighvatur Sveinsson
+
+tala1 = int(input("Sláðu inn tölu"))
+tala2 = int(input("Sláðu inn tölu"))
+summa = tala1 + tala2
+margfeldi = tala1 * tala2
+
+fornafn = input("Sláðu inn fornafn")
+eftirnafn = input("Sláðu inn eftirnafn")
+print("Halló", fornafn, eftirnafn)
